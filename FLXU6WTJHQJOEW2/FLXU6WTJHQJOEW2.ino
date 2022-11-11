133a380a4fbb609e2ec9df7353bde33cceff65ac
Max Struijs <maxstruys32@gmail.com>
2022-11-11 11:20:10 +0100

    Now the project works for us

--- FLXU6WTJHQJOEW2/FLXU6WTJHQJOEW2.ino
+++ FLXU6WTJHQJOEW2/FLXU6WTJHQJOEW2.ino
@@ -2,28 +2,6 @@
 // This code is for This code is for Wemos(ESP8266) based Energy monitoring Device
 // This code is a modified version of sample code from https://github.com/pieman64/ESPecoMon
 // Last updated on 30.05.2018
- 
-//#define BLYNK_DEBUG
-#define BLYNK_PRINT Serial
-#include <ArduinoOTA.h>
-#include <ESP8266WiFi.h>
-#include <BlynkSimpleEsp8266.h>
-#define CLOUD  // comment out for local server
-
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
- 
-#define OLED_RESET 0  // GPIO0
-Adafruit_SSD1306 display(OLED_RESET);
- 
-BlynkTimer timer;
-
-
-char auth[]       = "xxxx";
-char ssid[]       = "xxxx";
-char pass[]       = "xxxx";
-char server[]     = "blynk-cloud.com";    // ip or domain
-char myhostname[] = "Energy-Meter-V2.0";  // for OTA and router identification
 
 const int Sensor_Pin = A0;
 unsigned int Sensitivity = 185;   // 185mV/A for 5A, 100 mV/A for 20A and 66mV/A for 30A Module
@@ -62,11 +40,7 @@ void getACS712() {  // for AC
   Serial.print(String(power, 3)); 
   Serial.println(" W"); 
   Serial.print("  Bill Amount: INR"); 
-  Serial.println(String(bill_amount, 2));
-  Blynk.virtualWrite(V0, String (Wh));  // gauge 
-  Blynk.virtualWrite(V1, String(bill_amount, 2));
-  Blynk.virtualWrite(V2, String(power,2));
-  Blynk.virtualWrite(V3, String(Irms, 3));    
+  Serial.println(String(bill_amount, 2)); 
 }
 
 float getVPP()
@@ -91,93 +65,11 @@ float getVPP()
    result = ((maxValue - minValue) * Vcc) / 1024.0;  
    return result;
  }
- 
-BLYNK_WRITE(V4) {  // calibration slider 50 to 200
-    calibration = param.asInt();
-    }
-BLYNK_WRITE(V5) {  // set supply voltage slider 70 to 260
-    Supply_Voltage = param.asInt();
-    }
-BLYNK_WRITE(V6) {  // PF slider 60 to 100 i.e 0.60 to 1.00, default 85
-   pF = param.asInt();
-   }
-BLYNK_WRITE(V7) {  // Energy tariff slider 1 to 20, default 8 (Rs.8.0 / kWh)
-    energyTariff = param.asInt();
-}
-BLYNK_WRITE(V8) {  // set 5, 20 or 30A ACS712 sensor with menu
-    switch (param.asInt())
-    {
-      case 1: {       // 5A
-      Sensitivity = 185;
-        break;
-      }
-      case 2: {       // 20A
-       Sensitivity  = 100;
-        break;
-      }
-      case 3: {       // 30A
-       Sensitivity  = 66;
-        break;
-      }
-      default: {      // 5A
-      Sensitivity  = 185;
-      }
-    }
-}
-void displaydata() {
-  display.clearDisplay();
-  display.setTextColor(WHITE);
-  display.setTextSize(1);
-  display.setCursor(10, 3);
-  display.println("Pow  : "); 
-  display.setCursor(50, 3);  
-  display.println(power);
-  display.setCursor(85, 3);
-  display.println("W"); 
-  
-  display.setCursor(10, 13);
-  display.println("Ener : ");  
-  display.setCursor(50, 13);
-  display.println(Wh);
-  display.setCursor(85, 13);
-  display.println("Wh");
-
-  display.setCursor(10, 23);
-  display.println("Bill : "); 
-  display.setCursor(50, 23);
-  display.println(bill_amount);
-  display.setCursor(85, 23);
-  display.println("INR");
-  display.display();
-}
-
 
 void setup() {
-  display.begin();   
-  WiFi.hostname(myhostname);
-  Serial.begin(115200); 
-  Serial.println("\n Rebooted");
-  WiFi.mode(WIFI_STA);
-  #ifdef CLOUD
-    Blynk.begin(auth, ssid, pass);
-  #else
-    Blynk.begin(auth, ssid, pass, server);
-  #endif
-  while (Blynk.connect() == false) {}
-  ArduinoOTA.setHostname(myhostname);
-  ArduinoOTA.begin();
-  timer.setInterval(2000L, getACS712); // get data every 2s
-}
-
-BLYNK_CONNECTED(){
-  Blynk.syncAll();  
+ Serial.begin(9600);
 }
 
 void loop() {
-
-  displaydata();
-  Blynk.run();
-  ArduinoOTA.handle();
-  timer.run();
- 
+  getACS712();
 }
